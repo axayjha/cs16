b27cc145e6e7015e3c6e2911fac670ccb83b3f24
Akshay Jha <akshayjha@live.in>
2016-02-26 21:31:52 +0530

    1. Use of relational operators

--- Day 2/Prog1.c
+++ Day 2/Prog1.c
@@ -6,3 +6,42 @@
  * Akshay Anand
  * CSE 2nd Sem - St. Thomas' College of Engineering & Technology
  */
+
+ #include <stdio.h>
+
+ main()
+ {
+ 	int a=1, b=3, c=2, d=4, e =4, i;
+
+ 	i = a<b;
+ 	printf("%d\n", i);
+
+ 	i = b<=d;
+ 	printf("%d\n", i);
+
+ 	i = e>a;
+ 	printf("%d\n", i);
+
+ 	i = c>=e;
+ 	printf("%d\n", i);
+
+ 	i = d==e;
+ 	printf("%d\n", i);
+
+ 	i = d!=a;
+ 	printf("%d\n", i);
+
+
+ }
+
+
+/* Output
+
+   1
+   1
+   1
+   0
+   1
+   1
+
+   */
